aa4df520dac4d897ba40bcf20878d52875a9f830
Carlos Antonino <160295679+carloscastroslv@users.noreply.github.com>
2024-04-01 17:36:05 -0300

    Projeto inicial do Codelgniter

---
+++ app/Views/Produto.php
@@ -0,0 +1,6 @@
+<h1>Meus Produtos</h1>
+<p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse, rerum deserunt! Illum quo, molestiae iure 
+    aspernatur rerum repellat quasi excepturi temporibus iste laboriosam quam sapiente asperiores neque reiciendis
+    dolorem assumenda!
+</p>
